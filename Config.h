66660ada34b0d1ab8b450b20552f83fe426c46de
bubnikv <bubnikv@gmail.com>
2017-10-27 11:48:30 +0200

    Changed the name and version reported to HDSDR to ExtIO_Omnia-0.1. Lowered the buffer lengths for interfacing with HDSDR to a 5.33ms latency.

--- Config.h
+++ Config.h
@@ -1,14 +1,9 @@
 #pragma once
 
 #define EXTIO_EXPORTS		1
-#define HWNAME				"Demo-1.00"
-#define HWMODEL				"Demo ExtIO"
-#define SETTINGS_IDENTIFIER	"Demo-1.x"
-#define NUM_CARRIER         3
-#define LO_MIN				100000LL
-#define LO_MAX				7500000000LL
-#define LO_PRECISION		5000L
-// 15ms latency
-#define EXT_BLOCKLEN		(1024)			/* only multiples of 512 */
-
+#define HWNAME				"ExtIO_Omnia-0.1"
+#define HWMODEL				"ExtIO_Omnia"
+#define SETTINGS_IDENTIFIER	"ExtIO_Omnia-0.1"
+// 5.3ms latency
+#define EXT_BLOCKLEN		(512)			/* only multiples of 512 */
 #define SAMPLE_RATE			(96000)
